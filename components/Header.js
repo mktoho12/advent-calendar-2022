1c7eca5678ba0efb3fe5c7a371782107ebf7cc48
mktoho <mk@mktoho.org>
2022-11-30 17:43:36 +0900

    ヘッダーとか

--- components/Header.js
+++ components/Header.js
@@ -1,9 +1,18 @@
+import Image from 'next/image';
 import Link from 'next/link';
 
 export default function Header({ name }) {
   return (
     <header className="pt-20 pb-12">
-      <div className="w-12 h-12 rounded-full block mx-auto mb-4 bg-gradient-conic from-gradient-3 to-gradient-4" />
+      <div className="relative w-12 h-12 rounded-full block mx-auto mb-4 bg-gradient-conic from-gradient-3 to-gradient-4 overflow-hidden">
+        <Image
+          src="/mktoho.webp"
+          alt="mktoho"
+          className="absolute top-0 left-0 w-full aspect-square"
+          width={48}
+          height={48}
+        />
+      </div>
       <p className="text-2xl dark:text-white text-center">
         <Link href="/" legacyBehavior>
           <a>{name}</a>
--- pages/_document.js
+++ pages/_document.js
@@ -5,7 +5,16 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="en" className="theme-compiled">
-        <Head />
+        <Head>
+          <meta property="og:title" content="mktoho's portfolio" />
+          <meta property="og:type" content="website" />
+          <meta property="og:url" content="https://mktoho.dev/" />
+          <meta property="og:image" content="https://mktoho.dev/og.png" />
+
+          <link rel="icon" href="/favicon.ico" sizes="any" />
+          <link rel="icon" href="/icon.svg" type="image/svg+xml" />
+          <link rel="apple-touch-icon" href="icon.png" />
+        </Head>
         <body
           className={`antialiased text-lg bg-white dark:bg-gray-900 dark:text-white leading-base`}
         >
